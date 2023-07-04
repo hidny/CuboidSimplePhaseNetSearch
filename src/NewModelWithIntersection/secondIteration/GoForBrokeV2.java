e6b721d27cefa0b19fe41ca0b3b7b8b58a59e22a
hidny <mtardibuono@gmail.com>
2023-07-04 00:18:55 -0400

    Created a second version of the GoForBroke file

---
+++ src/NewModelWithIntersection/secondIteration/GoForBrokeV2.java
@@ -0,0 +1,91 @@
+package NewModelWithIntersection.secondIteration;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import DupRemover.BasicUniqueCheckImproved;
+import Model.Utils;
+
+public class GoForBrokeV2 {
+
+	public static void main(String[] args) {
+		
+		//int check46[][] = new int[][]{{5, 3, 1}, {7, 2, 1}};
+		//goForBroke(check46);
+		
+
+		//int check54[][] = new int[][]{{3, 3, 3}, {6, 3, 1}};
+		//goForBroke(check54);
+		
+		//int check58[][] = new int[][]{{5, 4, 1}, {9, 2, 1}};
+		//goForBroke(check58);
+		
+
+		//Nothing found
+		//int check62[][] = new int[][]{{5, 3, 2}, {7, 3, 1}};
+		//goForBroke(check62);
+
+		
+		int check70[][] = new int[][]{{5, 5, 1}, {8, 3, 1}, {11, 2, 1}};
+		goForBroke(check70);
+		
+		
+
+		int check78[][] = new int[][]{{5, 3, 3}, {7, 4, 1}, {9, 3, 1}};
+		goForBroke(check78);
+		
+
+		int check82[][] = new int[][]{{6, 5, 1}, {7, 3, 2}, {13, 2, 1}};
+		goForBroke(check82);
+		
+	}
+	
+	public static void goForBroke(int cuboids[][]) {
+		for(int i=1; i<cuboids.length; i++) {
+			if(Utils.getTotalArea(cuboids[0]) != Utils.getTotalArea(cuboids[i])) {
+				System.out.println("Error: the area of the two cuboids don't match.");
+				System.exit(1);
+			}
+		}
+		
+		ReallySimpleIntersectFinder.reallySimpleSearch(cuboids[0][0], cuboids[0][1], cuboids[0][2]);
+		//ReallySimpleIntersectFinder.reallySimpleSearch(2, 1, 1);
+		HashSet<BigInteger> origList = BasicUniqueCheckImproved.uniqList;
+		
+		for(int i=1; i<cuboids.length; i++) {
+			BasicUniqueCheckImproved.resetUniqList();
+			
+			ReallySimpleIntersectFinder.reallySimpleSearch(cuboids[i][0], cuboids[i][1], cuboids[i][2]);
+			//ReallySimpleIntersectFinder.reallySimpleSearch(2, 1, 1);
+			
+			Iterator<BigInteger> it = BasicUniqueCheckImproved.uniqList.iterator();
+			
+			System.out.println("Area of cuboids checked: " + Utils.getTotalArea(cuboids[0]));
+			
+			System.out.println("Looking for Solution that folds into 3 cuboids:");
+			boolean found = false;
+			while(it.hasNext()) {
+				BigInteger curSolution = it.next();
+				if(origList.contains(curSolution)) {
+					System.out.println("Found net that folds 3 cuboids!");
+					System.out.println("Solution code: " + curSolution);
+					found = true;
+				}
+			}
+			if(found == false) {
+				System.out.println("(Nothing found)");
+			} else {
+				System.exit(1);
+			}
+			
+			if(i + 1 <cuboids.length) {
+				HashSet<BigInteger> newList = BasicUniqueCheckImproved.uniqList;
+				
+				origList.addAll(newList);
+			}
+			
+		}
+	}
+
+}
