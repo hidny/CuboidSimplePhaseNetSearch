91fe1244d711b5c2506f61f4d8e3446c6976fe05
hidny <mtardibuono@gmail.com>
2023-11-07 00:18:45 -0500

    Started thinking about hwo to optimize the really slow and simple region split check. Found out that it returns true 70% of the time...

---
+++ src/SimplePhaseSearch/attemptAtRegionOptimization/ReallySimpleIntersectFinder7.java
@@ -0,0 +1,365 @@
+package SimplePhaseSearch.attemptAtRegionOptimization;
+
+import java.util.ArrayList;
+
+import DupRemover.BasicUniqueCheckImproved;
+import GraphUtils.PivotCellDescription;
+import GraphUtils.PivotCellDescriptionForNx1x1;
+import Model.Utils;
+import NewModel.firstIteration.Nx1x1CuboidToFold;
+
+public class ReallySimpleIntersectFinder7 {
+
+	public static void main(String[] args) {
+
+		//N:6 1 1: 223160
+		//reallySimpleSearch(6, 1, 1);
+
+		//N: 5 1 1: 23604
+		//reallySimpleSearch(5, 1, 1);
+		
+		//Found 34 unique solutions
+		//reallySimpleSearch(3, 2, 1);
+
+		//N: 7
+		//Found 6 unique solutions
+		//reallySimpleSearch(3, 3, 1);
+		
+
+		//N: 8
+		//Found 197 unique solutions
+		//reallySimpleSearch(5, 2, 1);
+		
+
+		//N: 9
+		//Found 33 unique solutions
+		//reallySimpleSearch(4, 3, 1);
+		
+
+		//N: 10
+		//Found 158 unique solutions
+		//reallySimpleSearch(3, 3, 2);
+		
+		
+		//N: 11
+		//Found 1401 unique solutions
+		/* From 11x1x1AND5x3x1Only1BottomNeighbourSolutions.txt:
+		 * Current UTC timestamp in milliseconds: 1675458353391
+Final number of unique solutions: 1401
+Current UTC timestamp in milliseconds: 1675458353391
+
+		 */
+		//reallySimpleSearch(5, 3, 1);
+
+		//Found 54 unique solutions
+		//reallySimpleSearch(7, 2, 1);
+		
+		
+		//N: 13
+		//Found 203 unique solutions (This matches what was found using the variant of redelmeier's algo)
+		//reallySimpleSearch(3, 3, 3);
+
+		//Found 77 unique solutions
+		//reallySimpleSearch(6, 3, 1);
+		
+		//N: 14
+		//Found 13264 unique solution. (3 minutes and 2 seconds on oct 14th)
+		//reallySimpleSearch(5, 4, 1);
+		
+		
+		//Found 355 unique solutions (40 seconds on oct 14th)
+		//reallySimpleSearch(9, 2, 1);
+
+		//N:15
+		//Found 507 unique solution. (6 minutes and 36 seconds on oct 14th)
+		//reallySimpleSearch(5, 3, 2);
+
+		//Found 31 unique solutions (almost 11 minutes 20 seconds on oct 14th) (14 minutes on nov 4th...)
+		//reallySimpleSearch(7, 3, 1);
+
+		//N: 17
+		//Found  unique solutions
+		//reallySimpleSearch(5, 5, 1);
+
+		//Found 54 unique solutions ( just over 2 hours on oct 12th and 1 hour and 34 minutes on oct 14th) (Only 14 minutes on Nov 4th)
+		reallySimpleSearch(8, 3, 1);
+		
+		//Found 89 unique solution. (17 minutes on oct 12th and 8 minutes and 36 seconds on oct 14th!) (4.5 minutes on nov 4th)
+		//3 minutes and 15 seconds on november 5th.
+		//reallySimpleSearch(11, 2, 1);
+
+		//N: 19
+		//Found 11290 unique solution. (8 hours and 30 minutes on oct 12th and 4 hours and 40 minutes on oct 14th)
+		//reallySimpleSearch(5, 3, 3);
+		
+		//Found 52 unique solution. (12 hours and 30 minutes on oct 12th and 5 hours and 30 minutes on oct 14th)
+		//reallySimpleSearch(7, 4, 1);
+		
+		//Found  unique solutions
+		//reallySimpleSearch(9, 3, 1);
+
+		//N: 20
+		//Found  unique solution
+		//reallySimpleSearch(6, 5, 1);
+
+		//Found 581 unique solution. (Almost 28 hours) (I got 581 in 8 hours and 20 minutes on October 28th)
+		//reallySimpleSearch(7, 3, 2);
+		
+		//Found 507 unique solutions (3 hours and 20 minutes on oct 12th) (1 hour and 30 minutes on Oct 28th) (47 minutes on Nov 4th)
+		//41 minutes on Nov 5th.(but I have 4 other programs running)
+		//reallySimpleSearch(13, 2, 1);
+		//
+
+		//--------
+		// N = 22
+		//{5, 5, 2}, {6, 3, 3)
+
+		//Found 163 unique solution. (Ran from Oct 12th 9PM to Oct 20th, 4pm) (or about 8 days)
+		//reallySimpleSearch(5, 5, 2);
+
+		//Found 105801 unique solution. (Ran from Oct 12th 9pm to Oct 18th, 7 pm) (or about 6 days)
+		//reallySimpleSearch(6, 3, 3);
+		
+		// N = 23 (4 other ones...)
+		/*
+		 * 5 x 4 x 3: 94
+7 x 5 x 1: 94
+11 x 3 x 1: 94
+15 x 2 x 1: 94
+		 */
+		
+		 //11,3,1:
+		//5, 4, 3
+
+		//Found  unique solutions
+		//reallySimpleSearch(5, 4, 3);
+
+		//Found  unique solutions
+		//reallySimpleSearch(7, 5, 1);
+
+		//Found 51 unique solutions (11 hours on November 5th (or maybe more...))
+		//reallySimpleSearch(11, 3, 1);
+		
+		//Found 150 unique solution. (14 hours and 32 minutes on October 16th)
+		//reallySimpleSearch(15, 2, 1);
+		
+
+		
+		// N = 25
+		/*
+7 x 3 x 3: 102
+9 x 3 x 2: 102
+12 x 3 x 1: 102
+		 */
+		//Found  unique solutions
+		//reallySimpleSearch(7, 3, 3);
+		
+		//Found  unique solutions
+		//reallySimpleSearch(9, 3, 2);
+		
+		//Found  unique solutions
+		//reallySimpleSearch(12, 3, 1);
+
+		// N = 26
+
+		//Found  unique solutions
+		//reallySimpleSearch(17, 2, 1);
+
+		//Found  unique solutions
+		//reallySimpleSearch(8, 5, 1);
+		
+		System.out.println("END");
+	}
+	
+
+	public static int debugBottomIndex = -1;
+	public static int debugAboveBottomIndex = -1;
+
+	public static void reallySimpleSearch(int a, int b, int c) {
+		
+		BasicUniqueCheckImproved.resetUniqList();
+		
+		
+		CuboidToFoldOnExtendedSimplePhase7 cuboidToBuild = new CuboidToFoldOnExtendedSimplePhase7(a, b, c);
+		
+		if(cuboidToBuild.getNumCellsToFill() % 4 != 2) {
+			System.out.println("ERROR: trying to find intersect between Nx1x1 solution and a cuboid solution that doesn't have a surface area that matches any Nx1x1 cuboid.");
+			return;
+		}
+
+		ArrayList<PivotCellDescription> startingPointsAndRotationsToCheck = PivotCellDescriptionForNx1x1.getUniqueRotationListsWithCellInfo(cuboidToBuild);
+		
+		long ret = 0;
+		
+		for(int i=22; i<startingPointsAndRotationsToCheck.size(); i++) {
+
+			int otherCuboidStartIndex =startingPointsAndRotationsToCheck.get(i).getCellIndex();
+			int otherCuboidStartRotation = startingPointsAndRotationsToCheck.get(i).getRotationRelativeToCuboidMap();
+			
+			System.out.println("Start recursion for other cuboid start index and rotation: (" + otherCuboidStartIndex + ", " + otherCuboidStartRotation + ")");
+			
+			System.out.println("Current UTC timestamp in milliseconds: " + System.currentTimeMillis());
+			
+			cuboidToBuild = new CuboidToFoldOnExtendedSimplePhase7(a, b, c);
+			cuboidToBuild.initializeNewBottomIndexAndRotation(otherCuboidStartIndex, otherCuboidStartRotation);
+			
+			debugBottomIndex = otherCuboidStartIndex;
+			debugAboveBottomIndex = cuboidToBuild.getIndexAboveIndex(otherCuboidStartIndex, otherCuboidStartRotation);
+			ret += findReallySimpleSolutionsRecursion(cuboidToBuild);
+			
+			System.out.println("Done with trying to intersect 2nd cuboid that has a start index of " + otherCuboidStartIndex + " and a rotation index of " + otherCuboidStartRotation +".");
+			System.out.println("Current UTC timestamp in milliseconds: " + System.currentTimeMillis());
+			
+		}
+		System.out.println("Done");
+		System.out.println("Found " + ret + " different solutions if we ignore symmetric solutions");
+		System.out.println();
+		System.out.println("Done using the 2nd iteration (using pre-computed long arrays)");
+		System.out.println("Found " + BasicUniqueCheckImproved.uniqList.size() + " unique solution.");
+
+	}
+	
+	public static int getNumLayers(CuboidToFoldOnExtendedSimplePhase7 cuboidToBuild) {
+		
+		return (cuboidToBuild.getNumCellsToFill() - 2) / 4;
+	}
+	
+	public static long findReallySimpleSolutionsRecursion(CuboidToFoldOnExtendedSimplePhase7 cuboidToBuild) {
+		return findReallySimpleSolutionsRecursionFirstLayer(cuboidToBuild, getNumLayers(cuboidToBuild));
+	}
+
+	public static final int FIRST_LEGAL_SIDE_BUMP_Nx1x1 = 3;
+	public static final int WIDTH_Nx1x1 = 4;
+	public static final int FIRST_CUR_LAYER_INDEX = 1;
+	
+	
+	public static long findReallySimpleSolutionsRecursionFirstLayer(CuboidToFoldOnExtendedSimplePhase7 cuboidToBuild, int numLayers) {
+		long ret = 0;
+		
+		if(numLayers == 0) {
+			return 1L;
+		}
+		
+		for(int sideBump=FIRST_LEGAL_SIDE_BUMP_Nx1x1; sideBump <FIRST_LEGAL_SIDE_BUMP_Nx1x1 + WIDTH_Nx1x1; sideBump++) {
+
+			//TODO: hide (sideBump - 6) inside a function...
+			if(CuboidToFoldOnExtendedSimplePhase7.LEVEL_OPTIONS[0][0 - (sideBump - 6)] != 1) {
+				System.out.println("OOPS in findReallySimpleSolutionsRecursionFirstLayer!");
+				System.exit(1);
+			}
+			
+			cuboidToBuild.addFirstLayer(sideBump);
+
+			ret += findReallySimpleSolutionsRecursion(cuboidToBuild, FIRST_CUR_LAYER_INDEX, numLayers, 0, false);
+
+			cuboidToBuild.leaveOnlyTheBottomCell();
+			
+		}
+		
+		
+		return ret;
+	}
+	
+	
+	public static long debugIterator = 0L;
+	public static final long DEBUG_PRINT = 100000000L;
+	public static long numSolutionsPrinted = 1L;
+	
+	
+	public static long findReallySimpleSolutionsRecursion(CuboidToFoldOnExtendedSimplePhase7 cuboidToBuild, int curLayerIndex, int numLayers, int prevLayerStateIndex,
+			boolean debugNope) {
+
+		debugIterator++;
+		//System.out.println("Iteration number: " + debugIterator);
+		
+
+		if(debugIterator % DEBUG_PRINT == 0) {
+			System.out.println("NumIterations = " + debugIterator);
+			cuboidToBuild.printCurrentStateOnOtherCuboidsFlatMap();
+		}
+		
+		long ret = 0;
+		
+		if(curLayerIndex == numLayers) {
+			
+			if(cuboidToBuild.isTopCellAbleToBeAddedFast()) {
+
+				for(int topSideBump=6; topSideBump<10; topSideBump++) {
+
+					if(cuboidToBuild.isTopCellAbleToBeAddedForSideBumpFast(topSideBump)) {
+						ret++;
+						
+						Nx1x1CuboidToFold reference = cuboidToBuild.createSolutionToPrint(topSideBump);
+						
+						if(
+							BasicUniqueCheckImproved.isUnique(Utils.getOppositeCornersOfNet(reference.setupBoolArrayNet()), reference.setupBoolArrayNet()) ){
+							
+							System.out.println("Unique solution found");
+							System.out.println("Num unique solutions found: " + BasicUniqueCheckImproved.uniqList.size());
+							
+							System.out.println(reference.toString());
+							
+							System.out.println("bottom index: " + debugBottomIndex);
+							System.out.println("above bottom index: " + debugAboveBottomIndex);
+							System.out.println();
+							System.out.println("top index: " + cuboidToBuild.debugGetTopCell());
+							System.out.println();
+							
+							System.out.println("Solution code: " + BasicUniqueCheckImproved.debugLastScore);
+							
+
+							if(debugIterator / numSolutionsPrinted > DEBUG_PRINT) {
+								numSolutionsPrinted++;
+								System.out.println("Other cuboid view:");
+								cuboidToBuild.printCurrentStateOnOtherCuboidsFlatMap();
+							}
+							
+							if(debugNope) {
+								System.out.println("Debug nope");
+								System.exit(1);
+							}
+							
+							//TODO: remove sanity check once confident:
+							cuboidToBuild.debugMakeSureCuboidIsFilledExceptForTop();
+						}
+					}
+				}
+				
+				if(ret > 0) {
+					System.out.println("Found " + ret + " places for top from this net:");
+					
+				}
+			}
+			return ret;
+		}
+		
+		if(cuboidToBuild.netShouldBeSolvedInOppositeDirection(curLayerIndex, prevLayerStateIndex)) {
+			
+			return 0;
+		}
+
+
+		//Go faster by iterating through the possible moves (i.e. (nextLayerState, sideBump) tuples)
+		for(int i=0; i<cuboidToBuild.nextLayerPossibilities[prevLayerStateIndex].length; i++) {
+
+			int nextLayerState = cuboidToBuild.nextLayerPossibilities[prevLayerStateIndex][i].i;
+			int sideBump = cuboidToBuild.nextLayerPossibilities[prevLayerStateIndex][i].j;
+			
+			if(cuboidToBuild.isNewLayerValidSimpleFast(nextLayerState, sideBump)) {
+				
+				cuboidToBuild.addNewLayerFast(nextLayerState, sideBump);
+				
+				if( ! cuboidToBuild.untouchableRegionCreatedAfterLayerAdded() ) {
+					ret += findReallySimpleSolutionsRecursion(cuboidToBuild, curLayerIndex + 1, numLayers, nextLayerState, debugNope);
+				}
+				
+				cuboidToBuild.removePrevLayerFast();
+				
+
+			}
+			
+		}
+		
+		return ret;
+	}
+}
---
+++ src/SimplePhaseSearch/attemptAtRegionOptimization/RegionSplitLogicSimple5Refined.java
@@ -0,0 +1,298 @@
+package SimplePhaseSearch.attemptAtRegionOptimization;
+
+import Coord.Coord2D;
+import Coord.CoordWithRotationAndIndex;
+
+//TODO: is this actually faster than RegionSplitLogicSimple?
+
+public class RegionSplitLogicSimple5Refined {
+
+	private CoordWithRotationAndIndex[][] neighbours;
+
+	public RegionSplitLogicSimple5Refined(CoordWithRotationAndIndex[][] neighbours) {
+		this.neighbours = neighbours;
+		this.tmpExplored = new boolean[this.neighbours.length];
+		this.tmpArray = new boolean[this.neighbours.length];
+		this.queue = new CustomDangerousQueue(this.neighbours.length);
+		
+
+		initNumCellsAbovePerLayer();
+		setupAnswerCellsAboveLayer();
+	}
+	
+	// Structure: [totalArea][NUM_ROTATION][NUM_LAYER_STATES][NUM_CELLS_PER_LAYER];
+
+	private int preComputedCellsAboveCurLayerMid[][][][];
+	private int preComputedCellsAboveCurLayerSide[][][][];
+	
+	private boolean tmpExplored[];
+	private boolean tmpArray[];
+	private CustomDangerousQueue queue;
+	
+	private long debug_return_true = 0;
+	private long debug_return_false = 0;
+	//TODO:
+	// This check is meant to save time by checking if there's a split before iterating through
+	// every sidebump:
+	public boolean untouchableRegionCreatedAfterLayerAdded(long curState[],
+			int lastLayerStateAdded,
+			int indexGroundedBelowLayerMid,
+			int rotationGroundedBelowLayerMid,
+			int indexGroundedBelowLayerSide,
+			int rotationGroundedBelowLayerSide
+	) {
+		
+		
+		for(int i=0; i<tmpArray.length; i++) {
+			this.tmpArray[i] = isCellIoccupied(curState, i);
+			this.tmpExplored[i] = false;
+		}
+		this.queue.resetQueue();
+	
+		for(int i=0; i<preComputedCellsAboveCurLayerMid[lastLayerStateAdded][indexGroundedBelowLayerMid][rotationGroundedBelowLayerMid].length; i++) {
+			
+			int tmpIndex = preComputedCellsAboveCurLayerMid[lastLayerStateAdded][indexGroundedBelowLayerMid][rotationGroundedBelowLayerMid][i];
+			if(! tmpArray[tmpIndex]) {
+				this.tmpExplored[tmpIndex] = true;
+				this.queue.add(tmpIndex);
+			}
+		}
+		
+		for(int i=0; i<preComputedCellsAboveCurLayerSide[lastLayerStateAdded][indexGroundedBelowLayerSide][rotationGroundedBelowLayerSide].length; i++) {
+			int tmpIndex = preComputedCellsAboveCurLayerSide[lastLayerStateAdded][indexGroundedBelowLayerSide][rotationGroundedBelowLayerSide][i];
+			if(! tmpArray[tmpIndex]) {
+				this.tmpExplored[tmpIndex] = true;
+				this.queue.add(tmpIndex);
+			}
+		}
+		
+		Integer v;
+		
+		while( ! this.queue.isEmpty()) {
+			
+			v = this.queue.poll();
+			
+			for(int i=0; i<NUM_NEIGHBOURS_PER_CELL; i++) {
+				
+				int neighbourIndex = this.neighbours[v.intValue()][i].getIndex();
+				
+				if( ! tmpArray[neighbourIndex] && ! tmpExplored[neighbourIndex]) {
+					tmpExplored[neighbourIndex] = true;
+					this.queue.add(neighbourIndex);
+				}
+				
+			}
+			
+		}
+
+		if((debug_return_true + debug_return_false) % 1000000L == 0) {
+			System.out.println("Basic region split function stats check: ");
+			System.out.println("debug_return_true: " + debug_return_true);
+			System.out.println("debug_return_false: " + debug_return_false);
+			
+			double perc = (100.0 * debug_return_true) / (1.0 * ((debug_return_true + debug_return_false)));
+			System.out.println("Percent true: " + perc);
+			
+		}
+		
+		for(int i=0; i<tmpArray.length; i++) {
+			if( ! tmpArray[i] && ! tmpExplored[i]) {
+
+				debug_return_true++;
+				return true;
+			}
+		}
+
+		debug_return_false++;
+		return false;
+		
+	}
+	
+	//Pre-compute:
+
+	public static final int NUM_LAYER_STATES = 7;
+
+	public static final int NUM_SIDE_BUMP_OPTIONS = 13;
+	public static final int NUM_ROTATIONS = 4;
+	public static final int NUM_CELLS_PER_LAYER = 4;
+	
+
+	public static final int ABOVE = 0;
+	public static final int RIGHT = 1;
+	public static final int BELOW = 2;
+	public static final int LEFT = 3;
+	
+	//TODO: this is only for going up through the middle...
+	// Try to also cover the case of going up on the side and going from top to bottom...
+
+	//TODO: handle the case of the 1st layer separately (i.e: bottom cell + 4 more cells)
+	
+	private int numCellsAbovePerLayerMid[];
+	private int numCellsAbovePerLayerSide[];
+	
+	public void initNumCellsAbovePerLayer() {
+
+		numCellsAbovePerLayerMid = new int[NUM_LAYER_STATES];
+		numCellsAbovePerLayerSide = new int[NUM_LAYER_STATES];
+				
+		for(int i=0; i<numCellsAbovePerLayerMid.length; i++) {
+			
+			boolean foundFirst = false;
+			int firstIndex = -1;
+			int lastIndex = -1;
+			
+			for(int j=0; j<CuboidToFoldOnExtendedSimplePhase7.CELLS_TO_ADD_BY_STATE_GOING_UP_MIDDLE[i].length; j++) {
+				
+				if(! foundFirst && CuboidToFoldOnExtendedSimplePhase7.CELLS_TO_ADD_BY_STATE_GOING_UP_MIDDLE[i][j] == 1) {
+					firstIndex = j;
+					foundFirst = true;
+
+				} else if(foundFirst && CuboidToFoldOnExtendedSimplePhase7.CELLS_TO_ADD_BY_STATE_GOING_UP_MIDDLE[i][j] == 0) {
+					lastIndex = j;
+				}
+			}
+			if(lastIndex == -1) {
+				lastIndex = 7;
+			}
+			numCellsAbovePerLayerMid[i] = lastIndex - firstIndex;
+		}
+		
+		//TODO: copy/paste code:
+		for(int i=0; i<numCellsAbovePerLayerSide.length; i++) {
+			
+			boolean foundFirst = false;
+			int firstIndex = -1;
+			int lastIndex = -1;
+			
+			for(int j=0; j<CuboidToFoldOnExtendedSimplePhase7.CELLS_TO_ADD_BY_STATE_GOING_UP_ON_SIDE[i].length; j++) {
+				
+				if(! foundFirst && CuboidToFoldOnExtendedSimplePhase7.CELLS_TO_ADD_BY_STATE_GOING_UP_ON_SIDE[i][j] == 1) {
+					firstIndex = j;
+					foundFirst = true;
+
+				} else if(foundFirst && CuboidToFoldOnExtendedSimplePhase7.CELLS_TO_ADD_BY_STATE_GOING_UP_ON_SIDE[i][j] == 0) {
+					lastIndex = j;
+				}
+				
+			}
+			if(lastIndex == -1) {
+				lastIndex = 7;
+			}
+			numCellsAbovePerLayerSide[i] = lastIndex - firstIndex;
+			
+		}
+		//END TODO copy/paste code
+		
+		
+	}
+	
+	public void setupAnswerCellsAboveLayer() {
+
+
+		// Structure: [NUM_LAYER_STATES][totalArea][NUM_ROTATION][NUM_CELLS_PER_LAYER];
+
+		preComputedCellsAboveCurLayerMid = new int[NUM_LAYER_STATES][neighbours.length][NUM_ROTATIONS][];
+		preComputedCellsAboveCurLayerSide = new int[NUM_LAYER_STATES][neighbours.length][NUM_ROTATIONS][];
+		
+		for(int layerState=0; layerState<NUM_LAYER_STATES; layerState++) {
+			
+			for(int index=0; index<neighbours.length; index++) {
+				for(int rotation=0; rotation<NUM_ROTATIONS; rotation++) {
+					
+					preComputedCellsAboveCurLayerMid[layerState][index][rotation] = new int[numCellsAbovePerLayerMid[layerState]];
+					preComputedCellsAboveCurLayerSide[layerState][index][rotation] = new int[numCellsAbovePerLayerSide[layerState]];
+					
+
+					Coord2D cur = new Coord2D(index, rotation);
+					for(int indexCurLayer=0; indexCurLayer<numCellsAbovePerLayerMid[layerState]; indexCurLayer++) {
+						
+						Coord2D above = tryAttachCellInDir(cur.i, cur.j, ABOVE);
+						
+						//TODO: I didn't bother making sure that the indexes in the list are distinct.
+						// I'll deal with this later.
+						preComputedCellsAboveCurLayerMid[layerState][index][rotation][indexCurLayer] = above.i;
+						
+						cur = tryAttachCellInDir(cur.i, cur.j, RIGHT);	
+					}
+
+					cur = new Coord2D(index, rotation);
+					for(int indexCurLayer=0; indexCurLayer<numCellsAbovePerLayerSide[layerState]; indexCurLayer++) {
+						
+						Coord2D above = tryAttachCellInDir(cur.i, cur.j, ABOVE);
+
+						//TODO: I didn't bother making sure that the indexes in the list are distinct.
+						// I'll deal with this later.
+						preComputedCellsAboveCurLayerSide[layerState][index][rotation][indexCurLayer] = above.i;
+						
+						cur = tryAttachCellInDir(cur.i, cur.j, RIGHT);
+						
+					}
+				}
+			}
+		}
+	}
+	
+	
+	private static final int NUM_NEIGHBOURS_PER_CELL = 4;
+	
+	private static final int NUM_BITS_IN_LONG = 64;
+	
+	public boolean isCellIoccupied(long curState[], int i) {
+		int indexArray = i / NUM_BITS_IN_LONG;
+		int bitShift = (NUM_BITS_IN_LONG - 1) - i - indexArray * NUM_BITS_IN_LONG;
+		
+		return ((1L << bitShift) & curState[indexArray]) != 0L;
+	}
+	
+	//TODO: copy/paste code:
+	public static final int NUM_LONGS_TO_USE = 2;
+	
+	private static long[] setImpossibleForAnswerSheet() {
+		
+		long ret[] = new long[NUM_LONGS_TO_USE];
+		
+		for(int i=0; i<ret.length; i++) {
+			ret[i] = -1L;
+		}
+		
+		return ret;
+	}
+	
+
+	private Coord2D tryAttachCellInDir(int curIndex, int rotationRelativeFlatMap, int dir) {
+		CoordWithRotationAndIndex neighbours[] = this.neighbours[curIndex];
+		
+		int neighbourIndex = (rotationRelativeFlatMap + dir) % NUM_NEIGHBOURS_PER_CELL;
+		curIndex = neighbours[neighbourIndex].getIndex();
+		rotationRelativeFlatMap = (rotationRelativeFlatMap + neighbours[neighbourIndex].getRot() + NUM_NEIGHBOURS_PER_CELL) % NUM_NEIGHBOURS_PER_CELL;
+		
+		return new Coord2D(curIndex, rotationRelativeFlatMap);
+	}
+	
+	
+	//TODO copy/paste code:
+
+	private long[] convertBoolArrayToLongs(boolean tmpArray[]) {
+		
+		//1st entry:
+		long ret[] = new long[NUM_LONGS_TO_USE];
+		
+		for(int i=0; i<ret.length; i++) {
+			ret[i] = 0;
+		}
+		
+		for(int i=0; i<tmpArray.length; i++) {
+			
+			if(tmpArray[i]) {
+				int indexArray = i / NUM_BITS_IN_LONG;
+				int bitShift = (NUM_BITS_IN_LONG - 1) - i - indexArray * NUM_BITS_IN_LONG;
+				
+				ret[indexArray] += 1L << bitShift;
+			}
+		}
+		
+		
+		return ret;
+	}
+
+}
